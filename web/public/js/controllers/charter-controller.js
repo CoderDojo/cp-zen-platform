67d62d9cf85024420a958c580abb43cdd643aa45
david-cahill <davidrichardcahill@gmail.com>
2015-03-31 16:55:40 +0100

    remove load charter from charter controller

--- web/public/js/controllers/charter-controller.js
+++ web/public/js/controllers/charter-controller.js
@@ -1,10 +1,8 @@
  'use strict';
 
 function cdCharterCtrl($scope, cdCharterService) {
-    cdCharterService.load(function(response) {
-      $scope.charterText = response;
-    });
-  }
+    
+}
 
 angular.module('cpZenPlatform')
     .controller('charter-controller', ['$scope', 'cdCharterService', cdCharterCtrl]);
