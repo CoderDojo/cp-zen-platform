5d720f7a148ca2fefc28c30f30a6f9df22a41162
david-cahill <davidrichardcahill@gmail.com>
2015-04-14 14:15:06 +0100

    script to add all geonames data to old dojos

--- scripts/add-geonames-data.js
+++ scripts/add-geonames-data.js
@@ -2,7 +2,8 @@
 
 var _ = require('lodash');
 var async = require('async');
-
+var http = require('http');
+var mongodb = require("mongodb"), objectID = mongodb.BSONPure.ObjectID;
 var seneca = require('seneca')({
   timeout: 10 * 60 * 1000
 });
@@ -16,8 +17,44 @@ seneca.use('mongo-store');
 
 seneca.ready(function() {
 
-  function run(cb) {
-    
+  function run(done) {
+    var dojosEntity = seneca.make$('cd/dojos');
+    dojosEntity.list$(function(err, response) {
+      if(err) return cb(err);
+      async.eachLimit(response, 5, function(dojo, cb) {
+        if(!dojo.coordinates) return cb();
+        if(objectID.isValid(dojo.id)) return cb(); //We don't need to update Dojo's with ObjectId as id type.
+        var latitude = dojo.coordinates.split(',')[0];
+        var longitude = dojo.coordinates.split(',')[1];
+        
+        http.get("http://api.geonames.org/countrySubdivisionJSON?lat="+latitude+"&lng="+longitude+"&level=4&username=davidc", function(res) {
+          var data = '';
+          res.setEncoding('utf8');
+          res.on("data", function(chunk) {
+            data += chunk;
+          });
+          res.on('end', function() {
+            data = JSON.parse(data);
+            console.log(JSON.stringify(data));
+            if(data.countryName) dojo.country = {countryName:data.countryName};
+            if(data.adminName1) dojo.state = {toponymName:data.adminName1};
+            if(data.adminName2) dojo.county = {toponymName:data.adminName2};
+            if(data.adminName3) dojo.city = {toponymName:data.adminName3};
+            var id = parseInt(dojo.id);
+            dojosEntity.remove$(id, function(err, response) {
+              if(err) return cb(err);
+              dojosEntity.save$(dojo, function(err, response) {
+                if(err) return cb(err);
+                console.log("saved dojo..." + JSON.stringify(response.id + ':' +response.name));
+                cb();
+              });
+            });
+          });
+        }).on('error', function(e) {
+          return cb(e);
+        });
+      }, done);
+    });
     
 
   }
