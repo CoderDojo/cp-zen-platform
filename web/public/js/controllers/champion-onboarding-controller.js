04a73111b8491aa26ad6b065e4070aa64553ab1f
david-cahill <davidrichardcahill@gmail.com>
2015-04-22 15:12:41 +0100

    update champion registration form

--- web/public/js/controllers/champion-onboarding-controller.js
+++ web/public/js/controllers/champion-onboarding-controller.js
@@ -1,8 +1,35 @@
  'use strict';
 
-function cdChampionOnboardingCtrl($scope) {
-    
+function cdChampionOnboardingCtrl($scope, auth) {
+  $scope.champion = {};
+
+  auth.get_loggedin_user(function(user) {
+    $scope.user = user;
+  });
+
+  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+  $scope.format = $scope.formats[0];
+
+  $scope.dateOptions = {
+    formatYear: 'yy',
+    startingDay: 1
+  };
+
+  $scope.open = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+
+    $scope.opened = true;
+  };
+
+  $scope.today = new Date();
+
+  $scope.answers = ['Yes', 'No'];
+
+  $scope.save = function(champion) {
+    console.log(JSON.stringify(champion));
+  }
 }
 
 angular.module('cpZenPlatform')
-    .controller('champion-onboarding-controller', ['$scope', cdChampionOnboardingCtrl]);
+    .controller('champion-onboarding-controller', ['$scope', 'auth', cdChampionOnboardingCtrl]);
