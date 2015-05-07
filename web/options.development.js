8690f2f9d13a31c6613b875176300c3038da5f81
sigismondm <sigismond.mclaughlin@nearform.com>
2015-05-07 08:57:59 -0500

    Add profiles configurations

--- web/options.development.js
+++ web/options.development.js
@@ -56,6 +56,7 @@ module.exports = _.defaults({
 
   client: [
     {type: 'web', host: '127.0.0.1', port: 10301, pin: 'role:cd-dojos,cmd:*'},
+    {type: 'web', host: '127.0.0.1', port: 10301, pin: 'role:cd-profiles,cmd:*'},
     {type: 'web', host: '127.0.0.1', port: 10302, pin: 'role:cd-countries,cmd:*'},
     {type: 'web', host: '127.0.0.1', port: 10302, pin: 'role:cd-geonames,cmd:*'},
     {type: 'web', host: '127.0.0.1', port: 10303, pin: 'role:cd-users,cmd:*'},
