79e8e96ce33571d67024c2f55fdd1ce4e100c348
iantocristian <iantocristian@yahoo.com>
2015-05-14 16:30:31 +0300

    endpoint for getting locale data

---
+++ web/controllers/locale/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var path = require('path');
+
+//var fs = require('fs');
+//var gettextParser = require('gettext-parser');
+
+var po2json = require('po2json');
+
+module.exports = function (router) {
+
+  router.get('/data', function (req, res) {
+    var locale = (res.locals && res.locals.context && res.locals.context.locality) || 'en_US';
+
+    //fs.readFile(path.join(__dirname, '../../locale/', locale, 'messages.po'), function(err, data) {
+    //  if (err) { throw err; }
+    //
+    //  var po = gettextParser.po.parse(data);
+    //
+    //  res.send(po.translations['']);
+    //});
+
+    po2json.parseFile(path.join(__dirname, '../../locale/', locale, 'messages.po'), {
+      format: 'jed',
+      domain: 'coder-dojo-platform'
+    }, function(err, data) {
+      if (err) { throw err; }
+      console.log(data);
+      res.send(data);
+    });
+
+  });
+
+};
