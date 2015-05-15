23be01435a02885ec514701ff4d754eef057f9cd
david-cahill <davidrichardcahill@gmail.com>
2015-05-15 16:28:10 +0100

    fix edit dojo functionality, return to dojo listing step if dojo is deleted

--- web/public/js/controllers/create-dojo-controller.js
+++ web/public/js/controllers/create-dojo-controller.js
@@ -11,18 +11,6 @@ function cdCreateDojoCtrl($scope, $window, $location, cdDojoService, cdCountries
 
  $scope.createDojoUrl = $state.current.url;
 
-  $scope.showCreateDojoForm = function() {
-    if($scope.user) {
-      //If user has the basic-user role, then this is their first Dojo. 
-      //They must first sign up to become a Champion before creating a Dojo.
-      var basicUser = _.find($scope.user.roles, function(role) { if(role === 'basic-user') { return true; } });
-      var mentorUser = _.find($scope.user.roles, function(role) { if(role === 'mentor') { return true; } });
-      if(basicUser || mentorUser) return false;
-      return true;
-    }
-    return false;
-  }
-
   cdCountriesService.listCountries(function(countries) {
     $scope.countries = _.map(countries, function(country) {
       return _.omit(country, 'entity$');
