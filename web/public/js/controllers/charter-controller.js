3fc21cfa61c281d5620dfce88934bcf5dfe1e5c4
david-cahill <davidrichardcahill@gmail.com>
2015-05-15 14:12:57 +0100

    fix bugs from weekly demo on 14/5

--- web/public/js/controllers/charter-controller.js
+++
@@ -1,8 +0,0 @@
- 'use strict';
-
-function cdCharterCtrl($scope, cdCharterService) {
-    
-}
-
-angular.module('cpZenPlatform')
-    .controller('charter-controller', ['$scope', 'cdCharterService', cdCharterCtrl]);
