1bc02ab0509bd51ef22c67eecedb3dd93c595d4d
iantocristian <iantocristian@yahoo.com>
2015-05-19 11:35:24 +0300

    locale cleanup

--- web/controllers/locale/index.js
+++ web/controllers/locale/index.js
@@ -2,23 +2,12 @@
 
 var path = require('path');
 
-//var fs = require('fs');
-//var gettextParser = require('gettext-parser');
-
 var po2json = require('po2json');
 
 module.exports = function (router) {
 
   router.get('/data', function (req, res) {
     var locale = (res.locals && res.locals.context && res.locals.context.locality) || 'en_US';
-
-    //fs.readFile(path.join(__dirname, '../../locale/', locale, 'messages.po'), function(err, data) {
-    //  if (err) { throw err; }
-    //
-    //  var po = gettextParser.po.parse(data);
-    //
-    //  res.send(po.translations['']);
-    //});
 
     var format = req.query['format'] || 'jed';
 
