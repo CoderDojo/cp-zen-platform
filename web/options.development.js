29e6355e7ddd9bf4206db2bc87fdc1f982b9c53d
Cristian Kiss <cristian.kiss@nearform.com>
2015-05-22 09:50:18 +0300

    postgresql-store is still used here for auth, so it's using the cp-users-development db only.(this will be moved to cp-users-service)

--- web/options.development.js
+++ web/options.development.js
@@ -19,6 +19,13 @@ function localhost() {
 
 module.exports = _.defaults({
   'agreement-version' : 2,
+  'postgresql-store': {
+    name: process.env.POSTGRES_NAME,
+    host: process.env.POSTGRES_HOST || localhost(),
+    port: process.env.POSTGRES_PORT || 5432,
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD
+  },
   auth: {
     restrict: ['/dashboard'],
     redirect:{
@@ -61,7 +68,8 @@ module.exports = _.defaults({
     {type: 'web', host: process.env.TARGETIP || '127.0.0.1', port: 10302, pin: 'role:cd-countries,cmd:*'},
     {type: 'web', host: process.env.TARGETIP || '127.0.0.1', port: 10302, pin: 'role:cd-geonames,cmd:*'},
     {type: 'web', host: process.env.TARGETIP || '127.0.0.1', port: 10303, pin: 'role:cd-users,cmd:*'},
-    {type: 'web', host: process.env.TARGETIP || '127.0.0.1', port: 10303, pin: 'role:cd-agreements,cmd:*'}
+    {type: 'web', host: process.env.TARGETIP || '127.0.0.1', port: 10303, pin: 'role:cd-agreements,cmd:*'},
+    {type: 'web', host: process.env.TARGETIP || '127.0.0.1', port: 10303, pin: 'role:user,cmd:*'}
   ]
 
 }, base);
