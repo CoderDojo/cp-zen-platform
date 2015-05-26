6830afa8795b613254dda4b06516a24faef481b9
Cristian Kiss <cristian.kiss@nearform.com>
2015-05-26 16:42:15 +0300

    removed database related scripts; some of them have been moved to their own service

--- scripts/database/pg/add-columns-to-cd-dojos.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE cd_dojos 
-ADD COLUMN dojo_lead_id character varying, 
-ADD COLUMN mailing_list smallint NOT NULL DEFAULT 0;
--- scripts/database/pg/add-columns-to-sys-user.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE sys_user
-ADD COLUMN terms_conditions_accepted boolean,
-ADD COLUMN mailing_list smallint NOT NULL DEFAULT 0;
