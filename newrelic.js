e330d8a391fe3139f45ac052d8d13325fdaf12bb
Cristian Kiss <cristian.kiss@nearform.com>
2015-06-08 10:02:56 +0300

    changed newrelic log path

--- newrelic.js
+++ newrelic.js
@@ -15,10 +15,24 @@ exports.config = {
   license_key: 'e324a157f25bbf3798247087b714ac1291f7dc7d',
   logging: {
     /**
-     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
-     * issues with the agent, 'info' and higher will impose the least overhead on
-     * production applications.
+     * Verbosity of the module's logging. This module uses bunyan
+     * (https://github.com/trentm/node-bunyan) for its logging, and as such the
+     * valid logging levels are 'fatal', 'error', 'warn', 'info', 'debug' and
+     * 'trace'. Logging at levels 'info' and higher is very terse. For support
+     * requests, attaching logs captured at 'trace' level are extremely helpful
+     * in chasing down bugs.
+     *
+     * @env NEW_RELIC_LOG_LEVEL
      */
-    level: 'info'
+    level: 'info',
+    /**
+     * Where to put the log file -- by default just uses process.cwd +
+     * 'newrelic_agent.log'. A special case is a filepath of 'stdout',
+     * in which case all logging will go to stdout, or 'stderr', in which
+     * case all logging will go to stderr.
+     *
+     * @env NEW_RELIC_LOG
+     */
+    filepath: '/var/log/newrelic/cp/newrelic_agent_zen_platform.log'
   }
 }
