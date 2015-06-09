91a6dfb606a70b94175aa5720b2be4ffbfe3600e
rmyers <rmyers@euro-pro.com>
2015-06-09 15:24:00 -0400

    base commit for redis shared session storage on localhost only

--- web/index.js
+++ web/index.js
@@ -4,12 +4,16 @@ require('newrelic');
 
 var bodyparser   = require('body-parser')
 var session      = require('express-session')
+var RedisStore = require('connect-redis')(session)
+
 
 
 var kraken = require('kraken-js')
 var app = require('express')()
 var env = process.env.NODE_ENV || 'development';
 var so = require('./options.' + env  + '.js');
+
+var sessionStore = new RedisStore()
 
 var options = {
   onconfig: function (config, next) {
@@ -26,7 +30,7 @@ require('./lib/dust-i18n.js');
 app.use(bodyparser.urlencoded({ extended: true }))
 app.use(bodyparser.json({ limit: so.bodyparser.json.limit }))
 
-app.use(session({ secret: 'seneca', name: 'CD.ZENPLATFORM', saveUninitialized: true, resave: true }))
+app.use(session({ store: sessionStore, secret: 'seneca', name: 'CD.ZENPLATFORM', saveUninitialized: true, resave: true }))
 
 app.listen(port, function (err) {
     console.log('[%s] Listening on http://localhost:%d', app.settings.env, port);
