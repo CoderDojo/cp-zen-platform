6b5dad9715d8010ab4c9cc530a86614bbb66215e
xqjibz <ccrider@whiterose.net>
2015-06-10 08:34:26 -0400

    Addresses PR comments, moving options back into base, and adding an environment variable for redis port, and expanding the redis host varaible choices for aws.

--- web/options.development.js
+++ web/options.development.js
@@ -48,11 +48,6 @@ module.exports = _.defaults({
     {type: 'web', host: process.env.TARGETIP || '127.0.0.1', port: 10303, pin: 'role:cd-users,cmd:*'},
     {type: 'web', host: process.env.TARGETIP || '127.0.0.1', port: 10303, pin: 'role:cd-agreements,cmd:*'},
     {type: 'web', host: process.env.TARGETIP || '127.0.0.1', port: 10303, pin: 'role:user,cmd:*'}
-  ],
-
-  redis: {
-    "host": process.env.TARGETIP || (process.env.DOCKER_HOST ? require('url').parse(process.env.DOCKER_HOST).hostname : '127.0.0.1'),
-    "port": 6379 // this isn't optioned
-  }
+  ]
 
 }, base);
