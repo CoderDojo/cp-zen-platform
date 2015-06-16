c7628e6b6b03506cba867459a1ca21f0094c62a1
kojiwakayama <koji.wakayama@nearform.com>
2015-06-16 12:27:10 +0200

    Add event form fields

--- web/public/js/controllers/dojo-event-form-controller.js
+++ web/public/js/controllers/dojo-event-form-controller.js
@@ -9,6 +9,22 @@
     if ($stateParams.eventId) {
       console.log('load event details');
     }
+
+    $scope.fromDate = new Date();
+    $scope.toDate = new Date();
+    $scope.minDate = new Date();
+
+    $scope.open = function($event, isOpen) {
+      $event.preventDefault();
+      $event.stopPropagation();
+      $scope[isOpen] = true;
+    };
+
+    $scope.time = new Date();
+    $scope.hstep = 1;
+    $scope.mstep = 15;
+    $scope.ismeridian = true;
+
   }
 
   angular.module('cpZenPlatform')
