f55c64bed970073a56475c8ab6839ef660ef80d0
Cristian Kiss <cristian.kiss@nearform.com>
2015-06-26 12:35:16 +0300

    added missing $q to my-dojos-controller

--- web/public/js/controllers/my-dojos-controller.js
+++ web/public/js/controllers/my-dojos-controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function cdMyDojosCtrl($scope, $window, $state, $stateParams, cdDojoService, $location, auth, tableUtils, alertService, $translate, AlertBanner) {
+function cdMyDojosCtrl($q, $scope, $window, $state, $stateParams, cdDojoService, $location, auth, tableUtils, alertService, $translate, AlertBanner) {
   $scope.itemsPerPage = 10;
   var currentUser;
   $scope.myDojosPageTitle = $translate.instant('My Dojos'); //sets breadcrumb page title
@@ -67,9 +67,9 @@ function cdMyDojosCtrl($scope, $window, $state, $stateParams, cdDojoService, $lo
            if(isChampion && isDojoAdmin && isDojoOwner) resolve(true);
            else resolve(false);
         }, function (err) {
-          reject( $translate.instant('Error loading user dojo entity') + ' <br /> ' + 
+          reject( $translate.instant('Error loading user dojo entity') + ' <br /> ' +
           (err.error || JSON.stringify(err)));
-        });  
+        });
       });
     }
 
@@ -145,4 +145,4 @@ function cdMyDojosCtrl($scope, $window, $state, $stateParams, cdDojoService, $lo
 }
 
 angular.module('cpZenPlatform')
-  .controller('my-dojos-controller', ['$scope', '$window', '$state', '$stateParams', 'cdDojoService', '$location', 'auth', 'tableUtils', 'alertService', '$translate', 'AlertBanner', cdMyDojosCtrl]);
+  .controller('my-dojos-controller', ['$q', '$scope', '$window', '$state', '$stateParams', 'cdDojoService', '$location', 'auth', 'tableUtils', 'alertService', '$translate', 'AlertBanner', cdMyDojosCtrl]);
