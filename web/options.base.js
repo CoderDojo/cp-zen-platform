078c68a5693df81c16b9431eaf9f47ee7e4240c2
Damian Beresford <dberesford@gmail.com>
2015-07-27 14:55:30 +0100

    Added DOCKER_HOST_IP

--- web/options.base.js
+++ web/options.base.js
@@ -46,7 +46,7 @@ module.exports = {
   },
 
   redis: {
-    "host": process.env.REDIS_HOST || process.env.TARGETIP || (process.env.DOCKER_HOST ? require('url').parse(process.env.DOCKER_HOST).hostname : '127.0.0.1'),
+    "host": process.env.REDIS_HOST || process.env.DOCKER_HOST_IP || process.env.TARGETIP || '127.0.0.1',
     "port": process.env.REDIS_PORT || 6379 // this is either configurable or in docker locally
   },
 
