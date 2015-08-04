dbc19fb41de85fa98a8ad5c8a10889679f89485b
Damian Beresford <dberesford@gmail.com>
2015-08-04 21:00:19 +0100

    CodeDeploy artifact support

---
+++ deploy/start-server.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+service cp-zen-platform start
--- start.sh
+++ start.sh
@@ -1,20 +1,19 @@
 #! /bin/bash
 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd );
-FILE="$DIR/web/config/$1".env && shift
-START="$DIR/$1" && shift
+FILE=$1
+START=$2
 USAGE="Usage: ./start.sh <config> <startscript> [startscript_opts]..."
 
 if [ ! -r $FILE ] ; then
-  echo "config file not found"
-  echo $USAGE
-  exit 1
+    echo "config file not found: $1"
+    echo $USAGE
+    exit 1
 fi
 
 if [ ! -r $START ] ; then
-  echo "start script not found"
-  echo $USAGE
-  exit 1
+    echo "start script not found: $2"
+    echo $USAGE
+    exit 1
 fi
 
 source $FILE
