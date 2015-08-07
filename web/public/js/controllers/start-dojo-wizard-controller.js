cd5d326b6b56c6d991efa184e1d154155f26cb72
Davey Moran <davey.moran@nearform.com>
2015-08-07 11:49:21 +0100

    adding locals for onboarding and manage pages

--- web/public/js/controllers/start-dojo-wizard-controller.js
+++ web/public/js/controllers/start-dojo-wizard-controller.js
@@ -39,8 +39,12 @@ function startDojoWizardCtrl($scope, $window, $state, $location, auth, alertServ
     } else {
       cdDojoService.getDojoConfig(function(json){
         $scope.dojoConfig = json;
-        $scope.dojoStages = json.dojoStages;
-        $scope.dojoStates = json.verificationStates;
+        $scope.dojoStages = _.map(json.dojoStages, function(item){
+          return { value: item.value, label: $translate.instant(item.label) };
+        });
+        $scope.dojoStates = _.map(json.verificationStates, function(item){
+          return { value: item.value, label: $translate.instant(item.label) };
+        });
       }, fail);
 
       var query = {userId: user.id};
