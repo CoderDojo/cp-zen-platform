d698ab7c55fb1692e1680f00472b0785e54abf63
david-cahill <davidrichardcahill@gmail.com>
2015-08-12 15:45:16 +0100

    use en_US if locality does not exist in locale folder

--- web/controllers/locale/index.js
+++ web/controllers/locale/index.js
@@ -3,6 +3,7 @@
 var path = require('path');
 var po2json = require('po2json');
 var languages = require('../../config/languages');
+var fs = require('fs');
 
 var controller = module.exports = [
 
@@ -12,10 +13,13 @@ var controller = module.exports = [
     handler: function (request, reply) {
     	var locale = (request.locals && request.locals.context && request.locals.context.locality) || 'en_US';
       locale = formatLocaleCode(locale);
+      if (!fs.existsSync(path.join(__dirname, '../../locale/', locale, 'messages.po'))) {
+        locale = 'en_US';
+      }
     	var format = request.query.format || 'jed';
     	po2json.parseFile(path.join(__dirname, '../../locale/', locale, 'messages.po'), {
-    	    format: format,
-    	    domain: 'coder-dojo-platform'
+        format: format,
+        domain: 'coder-dojo-platform'
     	}, reply);
     }
   },
