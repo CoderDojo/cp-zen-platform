4a5954dd79e938ed9b75bd81aa8e8fc758c4ab41
kamil-mech <kamil.mech@nearform.com>
2015-08-26 15:48:29 +0100

    fix dojo location issues

--- web/public/js/controllers/dojo-detail-controller.js
+++ web/public/js/controllers/dojo-detail-controller.js
@@ -12,12 +12,6 @@ function cdDojoDetailCtrl($scope, $state, $location, cdDojoService, cdUsersServi
 
 
   $scope.dojo = dojo;
-  if ($scope.dojo.place) {
-    $scope.dojo.place.name = _.find([$scope.dojo.place.name,
-                                     $scope.dojo.place.toponymName,
-                                     $scope.dojo.place.nameWithHierarchy],
-      function(obj) { return !!obj });
-  }
   $scope.model = {};
   $scope.markers = [];
   $scope.requestInvite = {};
