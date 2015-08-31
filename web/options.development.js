76a0feece2b45419a99138940833651504b0eb12
david-cahill <davidrichardcahill@gmail.com>
2015-08-31 16:56:56 +0100

    update tcp transports to http

--- web/options.development.js
+++ web/options.development.js
@@ -85,14 +85,14 @@ module.exports = _.defaults({
   },
 
   client: [
-    {type: 'http',  port: 10301, pin: 'role:cd-dojos,cmd:*'},
-    {type: 'http',  port: 10303, pin: 'role:cd-users,cmd:*'},
-    {type: 'http',  port: 10303, pin: 'role:cd-agreements,cmd:*'},
-    {type: 'http',  port: 10303, pin: 'role:cd-profiles,cmd:*'},
-    {type: 'http',  port: 10303, pin: 'role:cd-oauth2,cmd:*'},
-    {type: 'http',  port: 10303, pin: 'role:user,cmd:*'},
-    {type: 'http',  port: 10305, pin: 'role:cd-badges,cmd:*'},
-    {type: 'http',  port: 10306, pin: 'role:cd-events,cmd:*'}
+    {type: 'web',  port: 10301, pin: 'role:cd-dojos,cmd:*'},
+    {type: 'web',  port: 10303, pin: 'role:cd-users,cmd:*'},
+    {type: 'web',  port: 10303, pin: 'role:cd-agreements,cmd:*'},
+    {type: 'web',  port: 10303, pin: 'role:cd-profiles,cmd:*'},
+    {type: 'web',  port: 10303, pin: 'role:cd-oauth2,cmd:*'},
+    {type: 'web',  port: 10303, pin: 'role:user,cmd:*'},
+    {type: 'web',  port: 10305, pin: 'role:cd-badges,cmd:*'},
+    {type: 'web',  port: 10306, pin: 'role:cd-events,cmd:*'}
   ],
 
   redis: {
