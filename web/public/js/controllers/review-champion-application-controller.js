a4d0a994f79a53ef9ccb970a94a34b0ebbbd40d2
david-cahill <davidrichardcahill@gmail.com>
2015-09-02 12:53:07 +0100

    fix champion application details

--- web/public/js/controllers/review-champion-application-controller.js
+++ web/public/js/controllers/review-champion-application-controller.js
@@ -1,11 +1,12 @@
 'use strict';
 
-function cdReviewChampionApplicationCtrl($scope, $state, cdDojoService, $translate) {
+function cdReviewChampionApplicationCtrl($scope, $state, cdDojoService, $translate, utilsService) {
   var applicationId = $state.params.id;
+  var toTitleCase = utilsService.toTitleCase;
 
   cdDojoService.loadDojoLead(applicationId, function(response) {
   	var championApplication = response.application.championDetails;
-
+    var setupYourDojo = response.application.setupYourDojo;
   	var championDOB = moment(championApplication.dateOfBirth).format('MM/DD/YYYY');
   	championApplication.dateOfBirth = championDOB;
 
@@ -24,8 +25,25 @@ function cdReviewChampionApplicationCtrl($scope, $state, cdDojoService, $transla
       });
     });
   	$scope.championApplication = championApplication;
+
+    $scope.setupDojoSteps = [];
+    _.each(_.keys(setupYourDojo), function (setupDojoKey) {
+      var setupStep = {
+        key: setupDojoKey
+      };
+      if(setupDojoKey.slice(-4) !== 'Text') {
+        setupStep.title = toTitleCase(setupDojoKey);
+        setupStep.value = (setupYourDojo[setupDojoKey]) ? 'Yes' : 'No';
+        if(setupYourDojo.hasOwnProperty(setupDojoKey + 'Text')) {
+          setupStep.textArea = true;
+          setupStep.text = setupYourDojo[setupDojoKey + 'Text'];
+        }
+        $scope.setupDojoSteps.push(setupStep);
+      }
+    });
   });
+
 }
 
 angular.module('cpZenPlatform')
-    .controller('review-champion-application-controller', ['$scope', '$state', 'cdDojoService', '$translate', cdReviewChampionApplicationCtrl]);
+    .controller('review-champion-application-controller', ['$scope', '$state', 'cdDojoService', '$translate', 'utilsService', cdReviewChampionApplicationCtrl]);
