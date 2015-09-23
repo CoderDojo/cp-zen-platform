edd16136b084540e7d0ab7b43e932e42a01ff724
Ben Chapman <hello@benchapman.ie>
2015-09-23 15:30:49 +0100

    forumModerators now read from CDF_ADMINS environment variable instead of hardcoded

--- web/options.base.js
+++ web/options.base.js
@@ -45,6 +45,11 @@ var log = function () {
   };
 };
 
+function cdfAdmins() {
+  var admins = process.env.CDF_ADMINS || '';
+  return admins.split(',');
+}
+
 module.exports = {
   // purposely commented log: log(),
   actcache: {active:false},
@@ -100,17 +105,7 @@ module.exports = {
   webclient: {
     adultforum: process.env.ADULT_FORUM || 'http://localhost:4567',
     youthforum: process.env.YOUTH_FORUM || 'http://localhost:4567',
-    forumModerators: [
-      'ursula@coderdojo.org',
-      'giustina@coderdojo.org',
-      'pete@coderdojo.org',
-      'rosa@coderdojo.org',
-      'laura@coderdojo.org',
-      'mary@coderdojo.org',
-      'annabel@coderdojo.org',
-      'damian.beresford@nearform.com',
-      'david.cahill@nearform.com'
-    ]
+    forumModerators: cdfAdmins()
   }
 
 };
