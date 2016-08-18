7b611519a1491e0f79dec145f5c43e54a0c36d39
Daniel Brierton <daniel@brierton.me>
2016-08-18 14:18:13 +0100

    Added picture grid directive (#947)

---
+++ web/public/js/directives/cd-picture-grid/index.js
@@ -0,0 +1,10 @@
+(function () {
+  'use strict';
+
+  angular.module('cpZenPlatform').component('cdPictureGrid', {
+    bindings: {
+      items: '<'
+    },
+    templateUrl: '/directives/tpl/cd-picture-grid'
+  });
+})();
