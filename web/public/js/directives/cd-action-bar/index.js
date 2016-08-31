08185df050984cbf2e026766e1cc04828fee497d
Daniel Brierton <daniel@coderdojo.org>
2016-08-31 12:13:45 +0100

    Updated action bar to stick to the top of the footer when in view

--- web/public/js/directives/cd-action-bar/index.js
+++ web/public/js/directives/cd-action-bar/index.js
@@ -9,7 +9,25 @@
       },
       restrict: 'EA',
       templateUrl: '/directives/tpl/cd-action-bar',
-      transclude: true
+      transclude: true,
+      controller: ['$element', function ($element) {
+        var ctrl = this;
+
+        ctrl.$onInit = function () {
+          var actionBar = $element.find('.cd-action-bar');
+          $('#footer').on('inview', function (event, isInView) {
+            if (isInView) {
+              actionBar.removeClass('cd-action-bar--fixed');
+            } else {
+              actionBar.addClass('cd-action-bar--fixed');
+            }
+          });
+        };
+
+        ctrl.$onDestroy = function () {
+          $('#footer').off('inview');
+        };
+      }]
     });
 
 }());
