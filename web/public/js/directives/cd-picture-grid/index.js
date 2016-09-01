81f21524af3f83ad23bd1ca0cc0c6686b1322c55
Daniel Brierton <daniel@coderdojo.org>
2016-09-01 16:21:30 +0100

    Added actions for picture grid

--- web/public/js/directives/cd-picture-grid/index.js
+++ web/public/js/directives/cd-picture-grid/index.js
@@ -3,8 +3,37 @@
 
   angular.module('cpZenPlatform').component('cdPictureGrid', {
     bindings: {
-      items: '<'
+      items: '<',
+      actions: '<',
+      multipleSelection: '<'
     },
-    templateUrl: '/directives/tpl/cd-picture-grid'
+    templateUrl: '/directives/tpl/cd-picture-grid',
+    controller: function ($element) {
+      var ctrl = this;
+      ctrl.showActionBar = false;
+      ctrl.selectedItems = [];
+      var gridItems = null;
+      var selectedClass = 'cd-picture-grid__item--selected';
+
+      ctrl.handleSelection = function (e, item) {
+        if (ctrl.actions) {
+          e.preventDefault();
+          var $el = $(e.currentTarget);
+          if (ctrl.multipleSelection !== true && ctrl.selectedItems.length > 0) {
+            gridItems = gridItems || $element.find('.cd-picture-grid__item'); // so we only query the DOM once
+            gridItems.removeClass(selectedClass);
+            ctrl.selectedItems = [];
+          }
+          if ($el.hasClass(selectedClass)) {
+            $el.removeClass(selectedClass);
+            ctrl.selectedItems.splice(ctrl.selectedItems.indexOf(item), 1);
+          } else {
+            $el.addClass(selectedClass);
+            ctrl.selectedItems.push(item);
+          }
+          ctrl.showActionBar = ctrl.selectedItems.length > 0;
+        }
+      };
+    }
   });
 })();
