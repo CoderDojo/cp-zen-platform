15bbe6f4a2e802ddb6bb74d8aa08bff1e94ccef5
Wardormeur <wardormeur@gmail.com>
2016-09-02 13:49:07 +0100

    Enhance display of recurrent event by only showing upcoming events

--- web/public/js/directives/event/list/item/index.js
+++ web/public/js/directives/event/list/item/index.js
@@ -16,6 +16,22 @@ angular
             cdELI.isEmbedded = embedder.isEmbedded();
             cdELI.event.isPast = eventUtils.isEventInPast(_.last(cdELI.event.dates));
             cdELI.event = eventUtils.getFormattedDates(cdELI.event);
+            if (!cdELI.event.isPast && cdELI.event.type === 'recurring') {
+              cdELI.event.nextDate = void 0;
+              var nextDateIndex = void 0;
+              var eventIndex = 0;
+              while (eventIndex < cdELI.event.dates.length && _.isUndefined(nextDateIndex)) {
+                var date = cdELI.event.dates[eventIndex];
+                if (!eventUtils.isEventInPast(date)) {
+                  nextDateIndex = eventIndex;
+                }
+                eventIndex +=1;
+              }
+              if (nextDateIndex) {
+                cdELI.event.upcomingDates = cdELI.event.formattedDates.slice(nextDateIndex);
+                cdELI.event.nextDate = moment.utc(_.first(cdELI.event.upcomingDates).startTime).format('Do MMMM YY');
+              }
+            }
             cdELI.datesExpanded = false;
             cdELI.goTo = function() {
               embedder.redirectWrapper( function(){
