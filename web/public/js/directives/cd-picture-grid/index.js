ad288117fc44b0d7d2d1e328422db1c7488f0fcf
Daniel Brierton <daniel@brierton.me>
2016-09-05 16:14:47 +0100

    Picture grid actions (#989)
    * Added action bar directive
    
    * Updated action bar to stick to the top of the footer when in view
    
    * Fixed initial positioning of action bar
    
    * Added actions for picture grid
    
    * Moved to using variables from cd-common
    
    * Clear selection when the items are changed

--- web/public/js/directives/cd-picture-grid/index.js
+++ web/public/js/directives/cd-picture-grid/index.js
@@ -3,8 +3,47 @@
 
   angular.module('cpZenPlatform').component('cdPictureGrid', {
     bindings: {
-      items: '<'
+      items: '<',
+      actions: '<',
+      multipleSelection: '<'
     },
-    templateUrl: '/directives/tpl/cd-picture-grid'
+    templateUrl: '/directives/tpl/cd-picture-grid',
+    controller: function ($element) {
+      var ctrl = this;
+      ctrl.showActionBar = false;
+      ctrl.selectedItems = [];
+      var gridItems = null;
+      var selectedClass = 'cd-picture-grid__item--selected';
+
+      function clearSelection () {
+        gridItems = gridItems || $element.find('.cd-picture-grid__item'); // so we only query the DOM once
+        gridItems.removeClass(selectedClass);
+        ctrl.selectedItems = [];
+      }
+
+      ctrl.handleSelection = function (e, item) {
+        if (ctrl.actions) {
+          e.preventDefault();
+          var $el = $(e.currentTarget);
+          if (ctrl.multipleSelection !== true && ctrl.selectedItems.length > 0) {
+            clearSelection();
+          }
+          if ($el.hasClass(selectedClass)) {
+            $el.removeClass(selectedClass);
+            ctrl.selectedItems.splice(ctrl.selectedItems.indexOf(item), 1);
+          } else {
+            $el.addClass(selectedClass);
+            ctrl.selectedItems.push(item);
+          }
+          ctrl.showActionBar = ctrl.selectedItems.length > 0;
+        }
+      };
+
+      ctrl.onChanges = function (changes) {
+        if (changes.items) {
+          clearSelection();
+        }
+      };
+    }
   });
 })();
