741de85c8c9c9b0c138c43493494fc8e7a6b32a1
Benjamin Garcia <benjamin.garcia@acelys.fr>
2016-10-17 17:43:44 +0200

    feat(languages):update json

--- web/config/programmingLanguages.js
+++ web/config/programmingLanguages.js
@@ -1,29 +1,96 @@
 module.exports = [
-  {'text':'Scratch'},
-  {'text':'Java'},
-  {'text':'Python'},
-  {'text':'Ruby'},
-  {'text':'Perl'},
-  {'text':'PHP'},
-  {'text':'HTML'},
-  {'text':'Javascript'},
-  {'text':'NodeJS'},
-  {'text':'AngularJS'},
-  {'text':'C#'},
-  {'text':'C++'},
-  {'text':'Processing'},
-  {'text':'C'},
-  {'text':'Go'},
-  {'text':'CSS'},
-  {'text':'Lua'},
-  {'text':'Bash'},
-  {'text':'Haskell'},
-  {'text':'Scala'},
-  {'text':'Objective C'},
-  {'text':'SQL'},
-  {'text':'MySQL'},
-  {'text':'Swift'},
-  {'text':'R'},
-  {'text':'Clojure'},
+  {
+    'text': 'Scratch',
+    'image': '/img/languages/scratch.png'
+  },
+  {
+    'text': 'Java',
+    'image': '/img/languages/java.png'
+  },
+  {
+    'text': 'Python',
+    'image': '/img/languages/python.png'
+  },
+  {
+    'text': 'Ruby',
+    'image': '/img/languages/ruby.png'
+  },
+  {
+    'text': 'Perl',
+    'image': '/img/languages/perl.png'
+  },
+  {
+    'text': 'PHP',
+    'image': '/img/languages/php.png'
+  },
+  {
+    'text': 'HTML',
+    'image': '/img/languages/html.png'
+  },
+  {
+    'text': 'Javascript',
+    'image': '/img/languages/javascript.png'
+  },
+  {
+    'text': 'NodeJS',
+    'image': '/img/languages/nodejs.png'
+  },
+  {
+    'text': 'AngularJS',
+    'image': '/img/languages/angularjs.png'
+  },
+  {
+    'text': 'C#',
+    'image': '/img/languages/c_sharp.png'
+  },
+  {
+    'text': 'C++',
+    'image': '/img/languages/c_plus_plus.png'
+  },
+  {
+    'text': 'Processing',
+    'image': '/img/languages/processing.png'
+  },
+  {
+    'text': 'C',
+    'image': '/img/languages/c.png'
+  },
+  {
+    'text': 'Go',
+    'image': '/img/languages/golang.png'
+  },
+  {'text': 'CSS',
+    'image': '/img/languages/css.png'
+  },
+  {'text': 'Lua',
+    'image': '/img/languages/lua.png'
+  },
+  {'text': 'Bash',
+    'image': '/img/languages/bash.png'
+  },
+  {'text': 'Haskell',
+    'image': '/img/languages/haskell.png'
+  },
+  {'text': 'Scala',
+    'image': '/img/languages/scala.png'
+  },
+  {'text': 'Objective C',
+    'image': '/img/languages/objective_c.png'
+  },
+  {'text': 'SQL',
+    'image': '/img/languages/sql.png'
+  },
+  {'text': 'MySQL',
+    'image': '/img/languages/mysql.png'
+  },
+  {'text': 'Swift',
+    'image': '/img/languages/swift.png'
+  },
+  {'text': 'R',
+    'image': '/img/languages/r.png'
+  },
+  {'text': 'Clojure',
+    'image': '/img/languages/clojure.png'
+  }
 ];
 
