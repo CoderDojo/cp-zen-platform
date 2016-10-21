b642cb3862993efd26af42d5bb1bbf1d391d005d
Benjamin Garcia <benji.potter@gmail.com>
2016-10-21 16:51:22 +0200

    feat(languages): do one call and filter

--- web/public/js/services/programming-languages-service.js
+++ web/public/js/services/programming-languages-service.js
@@ -3,28 +3,8 @@
 function programmingLanguagesService($http) {
 
   return {
-    get: function (languages, onSuccess, onFailure) {
-      return $http.get('/programming-languages').then(
-        function (responseData) {
-          if(languages===null){
-            return _.map(responseData.data, function(langue){return langue.text});
-          } else {
-            var languageImageUrl = _.filter(responseData.data, function (langue) {
-              return _.isMatch(languages, langue);
-            }).map(function (language) {
-              return language.image
-            });
-            if (angular.isFunction(onSuccess)) {
-              onSuccess(languageImageUrl);
-            }
-          }
-        },
-        function () {
-          if (angular.isFunction(onFailure)) {
-            onFailure();
-          }
-        }
-      );
+    get: function () {
+      return $http.get('/programming-languages');
     }
   };
 }
