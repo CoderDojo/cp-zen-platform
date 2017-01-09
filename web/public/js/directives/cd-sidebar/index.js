259f91ddd96a930df45cc57118a204baeced2b70
Daniel Brierton <daniel@brierton.me>
2017-01-09 15:47:36 +0000

    Added my children page (#1066)
    * Added my children page
    
    * Changes after code review
    
    * Hide link for non-parents
    * Removed duplicate/accidental code
    * Replaced verbose sort comparitors with _.sortBy
    
    * Added new entries to messages.po + translateByKey filter
    
    * Updated cd-common for parent links

---
+++ web/public/js/directives/cd-sidebar/index.js
@@ -0,0 +1,34 @@
+;(function() {
+  'use strict';
+
+  angular
+    .module('cpZenPlatform')
+    .component('cdSidebar', {
+      bindings: {
+        header: '@',
+        tabs: '<',
+        baseState: '@'
+      },
+      restrict: 'E',
+      transclude: true,
+      templateUrl: '/directives/tpl/cd-sidebar',
+      controller: ['$state', '$scope', function ($state, $scope) {
+        var ctrl = this;
+
+        ctrl.getUiSrefForTab = function (tab) {
+          // Needed to use ui-sref so we can use ui-sref-active
+          // ui-sref expects a value of `state-name({param1: 'val1'})`
+          // We can use a scope var instead, hence the use of tab.stateParams here
+          return tab.state + '(tab.stateParams)';
+        };
+
+        ctrl.currentState = $state.current.name;
+
+        $scope.$watch(function () {
+          return $state.current.name;
+        }, function () {
+          ctrl.currentState = $state.current.name;
+        });
+      }]
+    });
+}());
--- web/public/js/directives/user/user-actions/index.js
+++ web/public/js/directives/user/user-actions/index.js
@@ -1,31 +1,14 @@
 ;(function() {
   'use strict';
 
-function cdUserActions(){
-    return {
-      scope: {
-        canEdit: '=',
-        user: '='
-      },
-      restrict: 'EA',
-      templateUrl: '/directives/tpl/user/user-actions',
-      controller: ['$scope', '$state', function ($scope, $state) {
-        var cdUA = this;
-        var conditionWatcher = $scope.$watch('canEdit', function(newCd, oldCd){
-          if (newCd) {
-            cdUA.canEdit = newCd;
-            conditionWatcher();
-          }
-        });
-        this.user = $scope.user;
-        this.currentState = $state.current.name;
-      }],
-      controllerAs: 'cdUA'
-    };
-  }
-
-angular
-    .module('cpZenPlatform')
-    .directive('cdUserActions', [cdUserActions]);
+  angular.module('cpZenPlatform').component('cdUserActions', {
+    bindings: {
+      canEdit: '=',
+      user: '='
+    },
+    restrict: 'EA',
+    templateUrl: '/directives/tpl/user/user-actions',
+    controllerAs: 'cdUA'
+  });
 
 }());
