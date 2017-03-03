ace1980b45c90beeb4bdb2dfeb523e3b78fa0324
Ben Skerritt <ben.skerritt77@gmail.com>
2017-03-03 08:49:27 +0000

    console.log removed :)

--- web/public/js/controllers/user-profile-controller.js
+++ web/public/js/controllers/user-profile-controller.js
@@ -197,7 +197,6 @@ function cdUserProfileCtrl($scope, $rootScope, $state, $window, auth, cdUsersSer
     var fail = function(){
       alertService.showError($translate.instant('An error has occurred while sending invitation') );
     };
-    console.log($scope);
     data.emailSubject = 'You have been invited to register as a parent/guardian on Zen, the CoderDojo community platform.';
     cdUsersService.inviteParent(data, win, fail);
   };
