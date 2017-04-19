3834299586f4885cf3e2abacc7a14ec3813fe660
Daniel Brierton <daniel@brierton.me>
2017-04-19 10:54:58 +0100

    Use cp-translations for i18n (#1118)
    * Use cp-translations instead of local translations
    
    * Added missing dependency
    
    * Updated circle.yml to build cp-translations in pre step

--- web/controllers/locale/index.js
+++ web/controllers/locale/index.js
@@ -4,6 +4,7 @@ var path = require('path');
 var po2json = require('po2json');
 var languages = require('../../config/languages');
 var fs = require('fs');
+var CpTranslations = require('cp-translations');
 
 var controller = module.exports = [
   {
@@ -12,11 +13,11 @@ var controller = module.exports = [
     handler: function (request, reply) {
     	var locale = (request.query && request.query.lang) || (request.locals && request.locals.context && request.locals.context.locality) || 'en_US';
       locale = formatLocaleCode(locale);
-      if (!fs.existsSync(path.join(__dirname, '../../locale/', locale, 'messages.po'))) {
+      if (!fs.existsSync(CpTranslations.getPoFilePath('messages.po', locale))) {
         locale = 'en_US';
       }
     	var format = request.query.format || 'jed';
-    	po2json.parseFile(path.join(__dirname, '../../locale/', locale, 'messages.po'), {
+    	po2json.parseFile(CpTranslations.getPoFilePath('messages.po', locale), {
         format: format,
         domain: 'coder-dojo-platform'
     	}, reply);
