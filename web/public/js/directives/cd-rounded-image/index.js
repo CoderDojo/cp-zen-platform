48e993a47d87f099c34c318f3550a7f02afbf5d7
Wardormeur <wardormeur@gmail.com>
2017-04-20 10:22:07 +0100

    Change srcFallback handling or rounded-image (#1115)
    * Fix 1179
    http head if done too soon isn't passing down every info to respect cors?
    
    * Apply Daniel's comment regarding scoping element research

--- web/public/js/directives/cd-rounded-image/index.js
+++ web/public/js/directives/cd-rounded-image/index.js
@@ -6,7 +6,7 @@
  * For upload, scope must contains upload function and return the new image path
  * @return {[type]} [description]
  */
-function cdRoundedImage(){
+  function cdRoundedImage () {
     return {
       scope: {
         src: '@?',
@@ -15,61 +15,59 @@ function cdRoundedImage(){
         alt: '@',
         size: '=?',
         edit: '=?',
-        upload: '=?',
-        format: '=?' // fallback for base64 to avoid the S3 request
+        upload: '=?'
       },
       restrict: 'EA',
       templateUrl: '/directives/tpl/cd-rounded-image',
-      controller: ['$scope', 'atomicNotifyService', '$translate', '$http', function ($scope, atomicNotifyService, $translate, $http) {
+      controller: ['$scope', 'atomicNotifyService', '$translate', '$http', '$timeout', '$element',
+      function ($scope, atomicNotifyService, $translate, $http, $timeout, $element) {
         this.srcFallback = $scope.srcFallback || 'https://placekitten.com/g/400/400';
         this.src = $scope.src ? $scope.src : this.srcFallback;
-        this.size = $scope.size? $scope.size : '200px';
+        this.size = $scope.size ? $scope.size : '200px';
         this.edit = $scope.edit ? $scope.edit : false;
         this.fullWidth = false;
-        this.format = $scope.format;
         this.saved = false;
         var cdRI = this;
 
-        var srcWatcher = $scope.$watch('src', function(newSrc, oldSrc){
+        var srcWatcher = $scope.$watch('src', function (newSrc, oldSrc) {
           if (newSrc) {
             cdRI.src = newSrc;
           }
         });
 
-        var idWatcher = $scope.$watch('srcId', function(newId, oldId){
+        var idWatcher = $scope.$watch('srcId', function (newId, oldId) {
           if (newId) {
             cdRI.srcId = newId;
             cdRI.src += $scope.srcId ? $scope.srcId : '';
-            idWatcher();
-            if(_.isUndefined(cdRI.format)) {
-              //  TODO: use a provider like cd-Entity-Service (i.e: cd-dojos-service=>getAvatar) to get the proper image
-              $http.head(cdRI.src).then(function successCallback(response) {
-                //Nothing to do, it all works as expected, the image is stored online
-              }, function errorCallback(response) {
-                //File doesn't exists, we should fallback
+            $timeout(function () {
+              var image = $element.find(cdRI.edit ? '.cd-rounded-image__img--edit-default'
+              : '.cd-rounded-image__img--display');
+
+              image.bind('error', function () {
                 cdRI.src = cdRI.srcFallback;
               });
-            }
+            });
+            idWatcher();
           }
         });
 
-        var altWatcher = $scope.$watch('alt', function(newAlt, oldAlt){
+        var altWatcher = $scope.$watch('alt', function (newAlt, oldAlt) {
           if (newAlt) {
             cdRI.srcId = newAlt;
             altWatcher();
           }
         });
 
-        this.toggleFullWidth = function (){
+        this.toggleFullWidth = function () {
           return this.fullWidth = !this.fullWidth;
-        }
+        };
         this.submit = function () {
           if (this.file) {
             this.upload(this.file)
-            .then(function(uploaded) {
+            .then(function (uploaded) {
               atomicNotifyService.info($translate.instant('Image successfully updated'));
               // Date.now is a hack to refresh the ng-src, because, yeah.
-              cdRI.src = uploaded + "#" + Date.now();
+              cdRI.src = uploaded + '#' + Date.now();
               cdRI.canSave = false;
             });
           }
@@ -78,9 +76,9 @@ function cdRoundedImage(){
         // upload on file select or drop
         this.upload = $scope.upload;
 
-        $scope.$on('$destroy', function(){
+        $scope.$on('$destroy', function () {
           srcWatcher();
-        })
+        });
       }],
       controllerAs: 'cdRI'
     };
@@ -89,5 +87,4 @@ function cdRoundedImage(){
 angular
     .module('cpZenPlatform')
     .directive('cdRoundedImage', [cdRoundedImage]);
-
 }());
