82855820a437207f584b1e96f26275024dac8847
Wardormeur <wardormeur@gmail.com>
2017-07-18 19:34:56 +0100

    Fix issue when directive-created input have no name

--- web/public/js/directives/cd-focus-invalid.js
+++ web/public/js/directives/cd-focus-invalid.js
@@ -46,7 +46,7 @@ function focusInvalid () {
       };
       // Continue or die condition when an element is found
       function checkElementValidity (prevV, newV, prev2, new2) {
-        if (controller[invalidElement.name].$valid) {
+        if (invalidElement.name && controller[invalidElement.name].$valid) {
           invalidElement = void 0;
           if (elementValidityWatcher) elementValidityWatcher();
         }
