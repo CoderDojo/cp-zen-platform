dfad15d927f8415b61fb98b0e8672fb5b92de6dc
butlerx <butlerx@notthe.cloud>
2017-08-21 19:57:53 +0100

    change patch command

--- deploy/kube.sh
+++ deploy/kube.sh
@@ -2,29 +2,28 @@
 
 set -e
 
-if [ "$CIRCLE_BRANCH" = "master" ]; then
+if [ "$GIT_BRANCH" = "master" ]; then
   DEP_VER=latest
   HOST=$PROD_HOST
-  CA_CERT=$PROD_CA_CERT
-  ADMIN_CERT=$PROD_ADMIN_CERT
-  ADMIN_KEY=$PROD_ADMIN_CERT
-elif [ "$CIRCLE_BRANCH" = "staging" ]; then
+  echo "$PROD_CA_CERT" | base64 -i --decode > ca.pem
+  echo "$PROD_ADMIN_KEY" | base64 -i --decode > admin-key.pem
+  echo "$PROD_ADMIN_CERT" | base64 -i --decode > admin.pem
+elif [ "$GIT_BRANCH" = "staging" ]; then
   DEP_VER=staging
   HOST=$STAGING_HOST
-  CA_CERT=$STAGING_CA_CERT
-  ADMIN_CERT=$STAGING_ADMIN_CERT
-  ADMIN_KEY=$STAGING_ADMIN_CERT
+  echo "$STAGING_CA_CERT" | base64 -i --decode  > ca.pem
+  echo "$STAGING_ADMIN_KEY" | base64 -i --decode > admin-key.pem
+  echo "$STAGING_ADMIN_CERT" | base64 -i --decode > admin.pem
 else
   exit 0
 fi
-sudo apt-get update
-sudo apt-get install --only-upgrade docker -y
-docker build --rm=false --build-arg DEP_VERSION=$DEP_VER -t coderdojo/cp-zen-platform:"$CIRCLE_SHA1" .
-docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
-docker push coderdojo/cp-zen-platform:"$CIRCLE_SHA1"
-sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl config set-cluster default-cluster --server=https://"${HOST}" --certificate-authority="${CA_CERT}"
-kubectl config set-credentials default-admin --certificate-authority="${CA_CERT}" --client-key="${ADMIN_KEY}" --client-certificate="${ADMIN_CERT}"
-kubectl config set-context default-system --cluster=default-cluster --user=default-admin
-kubectl config use-context default-system
-kubectl patch deployment zen -p '{"spec":{"template":{"spec":{"containers":[{"name":"zen","image":"coderdojo/cp-zen-platform:'"$CIRCLE_BUILD_SHA1"'"}]}}}}'
+docker build --rm=false --build-arg DEP_VERSION=$DEP_VER -t coderdojo/cp-zen-platform:"$GIT_SHA1" .
+docker login -u "$DOCKER_USER" -p "$DOCKER_PASS" -e "$DOCKER_EMAIL"
+docker push coderdojo/cp-zen-platform:"$GIT_SHA1"
+curl -O https://storage.googleapis.com/kubernetes-release/release/v1.6.1/bin/linux/amd64/kubectl
+chmod +x kubectl
+./kubectl config set-cluster default-cluster --server=https://"$HOST" --certificate-authority=ca.pem
+./kubectl config set-credentials default-admin --certificate-authority=ca.pem --client-key=admin-key.pem --client-certificate=admin.pem
+./kubectl config set-context default-system --cluster=default-cluster --user=default-admin
+./kubectl config use-context default-system
+./kubectl patch deployment zen -p '{"spec":{"template":{"spec":{"containers":[{"name":"zen","image":"coderdojo/cp-zen-platform:'"$GIT_SHA1"'"}]}}}}'
