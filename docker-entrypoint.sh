e583ac07298a9baa83c56b08d46be77528ee05b0
Daniel Brierton <daniel@brierton.me>
2017-08-23 17:53:52 +0100

    Stopped docker-entrypoint from npm link-ing to prevent npm installs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,9 @@
 #! /usr/bin/env sh
-cd /usr/src/cp-translations || exit
-npm link
-cd /usr/src/cp-zen-frontend || exit
-npm link
-cd /usr/src/app || exit
 # Dont chain this command to following commands as the post install is going to fail
 npm install
-npm link cp-translations && \
-npm link cp-zen-frontend && \
+rm -rf node_modules/cp-translations
+rm -rf node_modules/cp-zen-frontend
+ln -s /usr/src/cp-translations node_modules/cp-translations
+ln -s /usr/src/cp-zen-frontend node_modules/cp-zen-frontend
 ./node_modules/.bin/bower install --allow-root
 npm run dev
-
