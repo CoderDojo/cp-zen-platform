975ae229d6fec7c271feb9e5ae9e5759fbea6df7
butlerx <butlerx@notthe.cloud>
2017-09-04 15:26:32 +0100

    fix gulp

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 cd /usr/src/app || exit
 touch .yarn.sha1
 OLD_SHA=$(cat .yarn.sha1)
-NEW_SHA=$(sha1sum yarn.loack)
+NEW_SHA=$(sha1sum yarn.lock)
 if [ "$OLD_SHA" != "$NEW_SHA" ]; then
   echo "$NEW_SHA" > .yarn.sha1
   yarn
