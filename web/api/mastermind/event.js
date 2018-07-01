808ab9ffb88a45d86304d1f9cff1df06f382439d
Wardormeur <wardormeur@gmail.com>
2018-07-01 12:31:48 +0100

    Fix 'related' validation being extracted

--- web/api/mastermind/event.js
+++ web/api/mastermind/event.js
@@ -35,34 +35,6 @@ module.exports = [
   },
   {
     method: 'GET',
-    path: `${basePath}/dojos/{dojoId}/events/{eventId}`,
-    handler: eventHandlers.load(),
-    config: {
-      auth: auth.userIfPossible,
-      description: 'List events of a Dojo',
-      notes: 'Dojo\'s events',
-      tags: ['api', 'events'],
-      plugins: {
-        'hapi-swagger': {
-          responseMessages: [
-            { code: 400, message: 'Bad Request' },
-            { code: 200, message: 'OK' },
-          ],
-        },
-      },
-      validate: {
-        params: {
-          dojoId: Joi.string().guid().required(),
-          eventId: Joi.string().guid().required(),
-        },
-        query: Joi.object().keys({
-          related: Joi.string().valid(['sessions', 'sessions.tickets', 'sessions.tickets.applications']),
-        }),
-      },
-    },
-  },
-  {
-    method: 'GET',
     path: `${basePath}/dojos/{dojoId}/events`,
     handler: eventHandlers.get(),
     config: {
@@ -118,7 +90,36 @@ module.exports = [
           'query[beforeDate]': beforeDate,
           'query[afterDate]': afterDate,
           'query[utcOffset]': utcOffset,
-          related,
+          related: Joi.string().valid(['sessions', 'sessions.tickets']),
+        }),
+      },
+    },
+  },
+  // TODO : remove one of those load event
+  {
+    method: 'GET',
+    path: `${basePath}/dojos/{dojoId}/events/{eventId}`,
+    handler: eventHandlers.load(),
+    config: {
+      auth: auth.userIfPossible,
+      description: 'Load an event of a Dojo',
+      notes: 'Dojo\'s events',
+      tags: ['api', 'events'],
+      plugins: {
+        'hapi-swagger': {
+          responseMessages: [
+            { code: 400, message: 'Bad Request' },
+            { code: 200, message: 'OK' },
+          ],
+        },
+      },
+      validate: {
+        params: {
+          dojoId: Joi.string().guid().required(),
+          eventId: Joi.string().guid().required(),
+        },
+        query: Joi.object().keys({
+          related: Joi.string().valid(['sessions', 'sessions.tickets', 'sessions.tickets.applications']),
         }),
       },
     },
