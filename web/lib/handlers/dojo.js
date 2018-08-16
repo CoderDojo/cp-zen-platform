ba02811004c1d029034de84b820e5096e707c919
Wardormeur <wardormeur@gmail.com>
2018-08-16 14:03:59 +0100

    Force verification of a dojo with an email

--- web/lib/handlers/dojo.js
+++ web/lib/handlers/dojo.js
@@ -9,7 +9,16 @@ const verify = params => // eslint-disable-line no-unused-vars
       const user = req.user;
       return req.seneca.act({ role, ctrl: 'dojo', cmd: 'verify', id, verified, user },
         (err, res) => {
-          if (err) return cb(err);
+          if (err) {
+            const message = err.details.message;
+            let _err = err;
+            console.log(_err, message);
+            if (['Dojo not found', 'Dojo email is missing', 'Invalid verification scenario'].indexOf(message) > -1) {
+              _err = new Error(message);
+              _err.statusCode = 400;
+            }
+            return cb(_err);
+          }
           reply(res).code(200);
           cb();
         });
