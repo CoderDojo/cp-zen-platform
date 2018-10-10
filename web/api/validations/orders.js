cc56a0d287ecb37cff750c92d493d884b3ef16fb
Wardormeur <wardormeur@gmail.com>
2018-10-10 16:32:08 +0100

    Limit api uses-cases to know ones

--- web/api/validations/orders.js
+++ web/api/validations/orders.js
@@ -4,7 +4,22 @@ const Joi = require('joi');
 const fields = ['createdAt'];
 const query = queryFields(fields);
 const definitions = {
-  applications: Joi.array(),
+  applications: Joi.array().items(Joi.object().keys({
+    id: Joi.string().guid(),
+    ticketId: Joi.string().guid(),
+    name: Joi.string(),
+    dateOfBirth: Joi.date(),
+    eventId: Joi.string().guid(),
+    userId: Joi.string().guid(),
+    sessionId: Joi.string().guid(),
+    created: Joi.date(),
+    dojoId: Joi.string().guid(),
+    ticketId: Joi.string().guid(),
+    ticketName: Joi.string(),
+    ticketType: Joi.string(), // valid parent-guardian and such
+    notes: Joi.string(),
+    order_id: Joi.string().guid().optional(),
+  })),
 };
 module.exports = {
   base: query.base,
