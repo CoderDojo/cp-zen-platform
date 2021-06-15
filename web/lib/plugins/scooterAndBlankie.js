c62e9297bd07b74e05dfb2cefaf1375cb83bcb64
Greg Annandale <greg@raspberrypi.org>
2021-06-15 18:06:51 +0100

    Remove Intercom script and CSP entries

--- web/lib/plugins/scooterAndBlankie.js
+++ web/lib/plugins/scooterAndBlankie.js
@@ -10,7 +10,7 @@ exports.register = (server, options, next) => {
         options: {
           childSrc: "'none'",
           connectSrc:
-            "'self' https://*.intercom.io wss://*.intercom.io https://api-ping.intercom.io https://*.amazonaws.com https://www.eventbrite.com https://coderdojo.com https://learning-admin.raspberrypi.org https://forums.coderdojo.com",
+            "'self' https://*.amazonaws.com https://www.eventbrite.com https://coderdojo.com https://learning-admin.raspberrypi.org https://forums.coderdojo.com",
           defaultSrc: "'none'",
           fontSrc: "'self' http://fonts.gstatic.com https://fonts.gstatic.com",
           frameSrc: 'https://www.google.com https://www.youtube.com',
@@ -21,7 +21,7 @@ exports.register = (server, options, next) => {
           objectSrc: "'none'",
           reflectedXss: 'block',
           scriptSrc:
-            "'self' 'unsafe-inline' 'unsafe-eval' https://*.googleapis.com http://www.google-analytics.com https://www.google-analytics.com http://www.googletagmanager.com https://www.googletagmanager.com https://maps.gstatic.com https://www.gstatic.com https://widget.intercom.io https://js.intercomcdn.com https://www.google.com https://apis.google.com http://cdn.optimizely.com/js/3847550948.js http://www.googleadservices.com/pagead/conversion.js ",
+            "'self' 'unsafe-inline' 'unsafe-eval' https://*.googleapis.com http://www.google-analytics.com https://www.google-analytics.com http://www.googletagmanager.com https://www.googletagmanager.com https://maps.gstatic.com https://www.gstatic.com https://www.google.com https://apis.google.com http://cdn.optimizely.com/js/3847550948.js http://www.googleadservices.com/pagead/conversion.js ",
           styleSrc:
             "'self' 'unsafe-inline' http://fonts.googleapis.com https://fonts.googleapis.com",
         },
