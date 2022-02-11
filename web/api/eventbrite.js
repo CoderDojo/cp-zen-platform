9b2915093dd2e02a7999f1b8b44fbd54a8146a75
Izzy Smillie <isabelle.smillie@raspberrypi.org>
2022-02-11 13:06:46 +0000

    wip: adding organisation path to web/api

--- web/api/eventbrite.js
+++ web/api/eventbrite.js
@@ -82,6 +82,21 @@ exports.register = function(server, eOptions, next) {
         },
       },
     },
+    {
+      method: 'GET',
+      path: `${options.basePath}/users/me/organisations`,
+      handler: handlers.actHandlerNeedsUser('list', null, null, { ctrl: 'auth' }),
+      config: {
+        description: 'Get user organisation',
+        tags: ['api', 'dojos', 'eventbrite', 'organisation'],
+        auth: auth.apiUser,
+        plugins: {
+          'hapi-swagger': {
+            responseMessages: [{ code: 200, message: 'OK' }],
+          },
+        },
+      },
+    },
   ]);
   next();
 };
